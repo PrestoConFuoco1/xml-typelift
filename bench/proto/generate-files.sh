a72951156350ec1b797a2cab31cc87b53c76576a
Dmitry Krylov <dima@krylov.ws>
2020-02-10 10:17:26 +1000

    Generate very big files also

--- bench/proto/generate-files.sh
+++ bench/proto/generate-files.sh
@@ -7,7 +7,7 @@ stack build xml-typelift:exe:mkxml
 # place for generated files
 mkdir -p tmp
 
-for i in `seq 0 5` ; do
+for i in `seq 0 7` ; do
     size=`echo "32 * 2^$i" | bc`
     fn="tmp/customersOrders_`printf '%05i' $size`Mb.xml"
     echo "Generating ${fn}..."
