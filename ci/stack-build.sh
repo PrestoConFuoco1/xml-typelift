992cb97a7aacdd42cc85958b71bd18dce826d472
Dmitry Krylov <dima@krylov.ws>
2020-03-18 11:53:30 +1000

    Fix stack build

--- ci/stack-build.sh
+++ ci/stack-build.sh
@@ -4,8 +4,6 @@ source ci/common.sh
 
 # Build it
 message "Build it"
-# TODO: fix cache saving bug
-rm -rf .stack-work
 #export CI_GHC_ADDITIONAL_FLAGS="--system-ghc --allow-different-user"
 # Note: `--allow-different-user` flag is for debugging purpose,
 # when running this script locally in developer's working directory
