1988d5aac808c1c568ef6519402af7cbc1a1f563
Yuri Romanowski <yuri.romanowski@gmail.com>
2024-04-20 22:29:32 +0300

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 stack install xml-typelift:exe:xml-typelift-cli
-xml-typelift-cli --isogen-naming --generate-lenses --schema /home/yuri/prog/haskell/xml/parse1/xml/shiporder1.xsd --main --unsafe > /home/yuri/prog/haskell/xml/parse1/xml/log1 2> /home/yuri/prog/haskell/xml/parse1/xml/errlog1
+xml-typelift-cli --isogen-naming --generate-lenses --schema /home/yuri/prog/haskell/xml/parse1/xml/shiporder2.xsd --main --unsafe > /home/yuri/prog/haskell/xml/parse1/xml/log1 2> /home/yuri/prog/haskell/xml/parse1/xml/errlog1
 cp /home/yuri/prog/haskell/xml/parse1/xml/log1 /home/yuri/prog/haskell/xml/parse1/app/XMLSchema.hs
 
